8a2c63aeb6ef2af9199a810034deedcb1fab3a2b
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-09-07 12:09:35 -0300

    borra campo sala_id de la clase mensaje

--- SharedModels/ChatService/Mensaje.cs
+++ SharedModels/ChatService/Mensaje.cs
@@ -6,10 +6,7 @@ namespace SharedModels.ChatService
 {
     public class Mensaje : EntityBase
     {
-        public int SalaId { get; set; }
-        //public Sala Sala { get; set; }
-        public int ParticipanteId { get; set; }
-        //public Participante Participante { get; set; }
+        public Participante Participante { get; set; }
         public string Contenido { get; set; }
         public DateTime Hora_Fecha_Envio { get; set; }
         public string Url_Archivo { get; set; }
