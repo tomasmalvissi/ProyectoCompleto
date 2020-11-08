a94da821ef5d8bc8b79e6354e69a6844dae516e5
DSRamirez <d.s.ramirez@outlook.com>
2020-11-08 20:32:58 -0300

    Cambios

---
+++ RoomService/Controllers/ParticipanteController.cs
@@ -0,0 +1,34 @@
+﻿using DAL.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SharedModels.RoomService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipanteController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ParticipanteController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        //GET api/Patricipante
+        //Retorna la lista de participanes de la sala
+
+        [HttpGet("Participantes")]
+        public ActionResult<IEnumerable<Participante>> Get()
+        {
+            return context.Participantes.ToList();
+
+        }
+    }
+}
+
