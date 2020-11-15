c9aa7948bb7ed8c5e1209d07569b49137e092d3c
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-11-15 14:44:27 -0300

    corregido clase mensaje

--- SharedModels/ChatService/Mensaje.cs
+++ SharedModels/ChatService/Mensaje.cs
@@ -1,21 +1,19 @@
 ﻿using SharedModels.RoomService;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace SharedModels.ChatService
 {
     public class Mensaje : EntityBase
-
     {
-        public Participante Emisor { get; set; }
-        public Participante? Receptor { get; set; }
-        public Sala Sala { get; set; }
-
-        public string Contenido { get; set; }
+        public int? ParticipanteId { get; set; }
+        public Participante Participante { get; set; }
+        public string? Contenido { get; set; }
         public DateTime Hora_Fecha_Envio { get; set; }
-        public string Url_Archivo { get; set; }
+        public string? Url_Archivo { get; set; }
         public bool Privado { get; set; }
     }
 }
