11b2f7084b96d358e801bea35a555a10fc12edfa
Tomás Malvissi <50890384+tomasmalvissi@users.noreply.github.com>
2020-11-21 01:05:16 -0300

    add empresas controller

---
+++ UserService/Controllers/EmpresasController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAL.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SharedModels.UserService;
+
+namespace UserService.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        private readonly DataContext _context;
+
+        public EmpresasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empresas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Empresa>>> GetEmpresas()
+        {
+            return await _context.Empresas.ToListAsync();
+        }
+
+        // GET: api/Empresas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empresa>> GetEmpresa(int id)
+        {
+            var empresa = await _context.Empresas.FindAsync(id);
+
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            return empresa;
+        }
+
+
+        // PUT: api/Empresas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpresa(string id, Empresa empresa)
+        {
+            empresa.DateModified = DateTime.Now;
+            _context.Entry(empresa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpresaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Empresas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Empresa>> DeleteEmpresa(int id)
+        {
+            var empresa = await _context.Empresas.FindAsync(id);
+            if (empresa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empresas.Remove(empresa);
+            await _context.SaveChangesAsync();
+
+            return empresa;
+        }
+
+        private bool EmpresaExists(string id)
+        {
+            return _context.Empresas.Any(e => e.IdentityUsuario.Id == id);
+        }
+    }
+}
