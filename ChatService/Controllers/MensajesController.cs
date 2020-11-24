1a6674f89a0176aa33bb4c2c48d3db35072dc568
JoelPallero <estebanjoelpallero@hotmail.com>
2020-11-24 12:05:08 -0300

    Se quita información demás.

--- ChatService/Controllers/MensajesController.cs
+++ ChatService/Controllers/MensajesController.cs
@@ -49,28 +49,18 @@ namespace ChatService.Controllers
             return _context.Mensajes.Where(s => s.SalaId == idsala).CountAsync();
         }
 
-        // GET: api/Mensajes/CantidadPorPart/?
-        [HttpGet("CantidadPorParticipante/{idsala}")]
+        // GET: api/Mensajes/CantPorPart/?
+        [HttpGet("CantPorPart/{idsala}")]
         public dynamic GetCountPorPart(int idsala)
         {
-            //Devuelve los Id de los que enviaron msj y la cantidad que envió cada uno
             return (from m in _context.Mensajes
                     where m.SalaId == idsala
                     group m by m.EmisorId into grupo
-
                     select new
                     {
                         Emisor = grupo.Key,
                         TotalEnviados = grupo.Count()
                     }).ToList();
-
-
-            //var contador = _context.Mensajes
-            //    .Where(s => s.SalaId == idsala)
-            //    .GroupBy(p => new { p.EmisorId })
-            //    .Select(r => new { r.Key.EmisorId, Total = r.Count() }).Count();
-
-            //return contador;
         }
 
         // GET: api/Mensajes/bysala/5
